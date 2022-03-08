1b7331ffe6955b781ea7ab0c99f09f56ecfd2ad3
Jacob <jacobhtnguyen@gmail.com>
2022-03-08 15:19:12 -0500

    Initial commit

---
+++ src/main/java/com/harukine/userslist/UsersListApplication.java
@@ -0,0 +1,13 @@
+package com.harukine.userslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harukine/userslist/UsersListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harukine.userslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
