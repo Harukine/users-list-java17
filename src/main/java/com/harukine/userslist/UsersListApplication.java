7acda8f55d7e9def5e784f5d5c03fb43f91dc5f3
Jacob <jacobhtnguyen@gmail.com>
2022-03-08 16:52:38 -0500

    added commmand to populate database

--- src/main/java/com/harukine/userslist/UsersListApplication.java
+++ src/main/java/com/harukine/userslist/UsersListApplication.java
@@ -1,7 +1,10 @@
 package com.harukine.userslist;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class UsersListApplication {
@@ -10,4 +13,13 @@ public class UsersListApplication {
         SpringApplication.run(UsersListApplication.class, args);
     }
 
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+                User user = new User(name, name.toLowerCase() + "@domain.com");
+                userRepository.save(user);
+            });
+            userRepository.findAll().forEach(System.out::println);
+        };
+    }
 }
