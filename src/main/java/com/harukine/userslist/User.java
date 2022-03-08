6becf999f58f4bb4e463b1ea8b1c67735a4c1271
Jacob <jacobhtnguyen@gmail.com>
2022-03-08 17:03:03 -0500

    generated toString method

--- src/main/java/com/harukine/userslist/User.java
+++ src/main/java/com/harukine/userslist/User.java
@@ -24,4 +24,13 @@ public class User {
     public void setId(Long id) {
         this.id = id;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
