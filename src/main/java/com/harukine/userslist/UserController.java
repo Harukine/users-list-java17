f39fd816e7727727deb1fafb040b57329a81c7d6
Jacob <jacobhtnguyen@gmail.com>
2022-03-08 15:54:45 -0500

    created user controller

---
+++ src/main/java/com/harukine/userslist/UserController.java
@@ -0,0 +1,26 @@
+package com.harukine.userslist;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+
+    // standard constructors
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    void addUser(@RequestBody User user) {
+        userRepository.save(user);
+    }
+}
