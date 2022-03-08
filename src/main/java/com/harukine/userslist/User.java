8ee18abdd5b9860b2049d6d0ed5f08af5db98726
Jacob <jacobhtnguyen@gmail.com>
2022-03-08 15:35:58 -0500

    created user entity

---
+++ src/main/java/com/harukine/userslist/User.java
@@ -0,0 +1,27 @@
+package com.harukine.userslist;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+    private String email;
+
+    public User() {
+    }
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
