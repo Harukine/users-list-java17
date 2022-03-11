dec907dbb19d8a1f215a698340728056ea7e3c68
Jacob <jacobhtnguyen@gmail.com>
2022-03-10 23:12:44 -0500

    implemented user service

--- src/main/webapp/src/app/user-service.service.ts
+++ src/main/webapp/src/app/user-service.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
+import {Observable} from "rxjs";
 
-@Injectable({
-  providedIn: 'root'
-})
-export class UserServiceService {
 
-  constructor() { }
+@Injectable()
+export class UserService {
+
+  private readonly usersUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.usersUrl = 'http://localhost:8080/users';
+  }
+
+  public findAll(): Observable<User[]> {
+    return this.http.get<User[]>(this.usersUrl);
+  }
+
+  public save(user: User) {
+    return this.http.post<User>(this.usersUrl, user);
+  }
 }
